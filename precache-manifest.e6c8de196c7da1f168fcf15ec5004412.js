0ba899da910a992e639af8077c61b6c483460d0b
NelliPestych <NelliPestych@bigmir.net>
2020-09-05 17:31:00 +0300

    Updates

---
+++ precache-manifest.e6c8de196c7da1f168fcf15ec5004412.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa175e2a895249e7ca6964fc5e14901",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "31417b2071ba1ddad199",
+    "url": "/goit-react-hw-08-phonebook/static/css/7.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9f3a6108ef1795002029",
+    "url": "/goit-react-hw-08-phonebook/static/css/contacts-page.35f1b40a.chunk.css"
+  },
+  {
+    "revision": "391f1352242a8e7617bc",
+    "url": "/goit-react-hw-08-phonebook/static/css/home-page.315d959f.chunk.css"
+  },
+  {
+    "revision": "ed40b9e173f4022b006d",
+    "url": "/goit-react-hw-08-phonebook/static/css/login-page.bc603d63.chunk.css"
+  },
+  {
+    "revision": "ec3f509f10182937fabd",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.fda3b230.chunk.css"
+  },
+  {
+    "revision": "3664a1adc1e438e989d6",
+    "url": "/goit-react-hw-08-phonebook/static/css/registration-page.915707e8.chunk.css"
+  },
+  {
+    "revision": "479cd970d9ebb31f3240",
+    "url": "/goit-react-hw-08-phonebook/static/js/0.151e8af7.chunk.js"
+  },
+  {
+    "revision": "31417b2071ba1ddad199",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.40a26e33.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.40a26e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38eb872dc2c99b5f548b",
+    "url": "/goit-react-hw-08-phonebook/static/js/8.0092b988.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/goit-react-hw-08-phonebook/static/js/8.0092b988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3a6108ef1795002029",
+    "url": "/goit-react-hw-08-phonebook/static/js/contacts-page.4aef2476.chunk.js"
+  },
+  {
+    "revision": "391f1352242a8e7617bc",
+    "url": "/goit-react-hw-08-phonebook/static/js/home-page.8cc46464.chunk.js"
+  },
+  {
+    "revision": "ed40b9e173f4022b006d",
+    "url": "/goit-react-hw-08-phonebook/static/js/login-page.f9a03e2a.chunk.js"
+  },
+  {
+    "revision": "ec3f509f10182937fabd",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.587d73a5.chunk.js"
+  },
+  {
+    "revision": "3664a1adc1e438e989d6",
+    "url": "/goit-react-hw-08-phonebook/static/js/registration-page.815676db.chunk.js"
+  },
+  {
+    "revision": "e907e1ecd9a9fc8ae54d",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.1490be83.js"
+  }
+]);
---
+++ static/js/home-page.8cc46464.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[2],{197:function(e,t,a){e.exports={container:"home_container__1nlH-",title:"home_title__15DmN",p:"home_p__IZtIr"}},216:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(197),i=a.n(c);t.default=function(){return o.a.createElement("div",{className:i.a.container},o.a.createElement("h2",{className:i.a.title},"Home Page"),o.a.createElement("p",{className:i.a.p},"Welcome!"))}}}]);
+//# sourceMappingURL=home-page.8cc46464.chunk.js.map
